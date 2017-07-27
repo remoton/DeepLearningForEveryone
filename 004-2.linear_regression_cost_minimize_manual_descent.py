798bac0813e845fba4d6f5f952cd965f375dcc71
remoton <remoton@naver.com>
2017-07-27 20:45:03 +0900

    Add files via upload

---
+++ 004-2.linear_regression_cost_minimize_manual_descent.py
@@ -0,0 +1,27 @@
+import tensorflow as tf
+
+x_data=[1,2,3]
+y_data=[1,2,3]
+
+W=tf.Variable(tf.random_normal([1]), name='weight')
+X=tf.placeholder(tf.float32, shape=[None])
+Y=tf.placeholder(tf.float32, shape=[None])
+
+hypothesis=W*X
+
+cost=tf.reduce_mean(tf.square(hypothesis-Y))
+
+# define manual descent alogorithm
+learning_rate=0.1
+gradient=tf.reduce_mean((W*X-Y)*X)
+descent=W-learning_rate*gradient
+update=W.assign(descent)
+
+sess=tf.Session()
+sess.run(tf.global_variables_initializer())
+
+for step in range(21):
+	sess.run(update, feed_dict={X:x_data, Y:y_data})
+	print(step, sess.run(cost, feed_dict={X:x_data, Y:y_data}), sess.run(W))
+
+
